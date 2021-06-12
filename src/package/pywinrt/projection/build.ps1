f4dd10b0f8cad9e4ed2a7956d4ee65ca1f9dba8a
David Lechner <david@pybricks.com>
2021-06-12 18:39:05 -0500

    python: add parameters to build script
    This make $buildType a parameter and changes the default to "Debug". And it adds a new parameter for the Python version.

--- src/package/pywinrt/projection/build.ps1
+++ src/package/pywinrt/projection/build.ps1
@@ -1,10 +1,16 @@
-$buildType = "Release"
+param (
+    [Parameter(Mandatory=$false)]
+    [string]$buildType = "Debug",
+
+    [Parameter(Mandatory=$false)]
+    [string]$pythonVersion = "3.9"
+)
 
 $repoRootPath = (get-item $PSScriptRoot).parent.Parent.parent.Parent.FullName.Replace('\', '/')
 $sourcePath = $PSScriptRoot.Replace('\', '/')
 $buildPath = "$repoRootPath/_build/py-projection/$env:VSCMD_ARG_TGT_ARCH-$buildType"
 
-cmake -S $sourcePath "-B$buildPath" -GNinja "-DCMAKE_BUILD_TYPE=$buildType" -DCMAKE_C_COMPILER=cl -DCMAKE_CXX_COMPILER=cl
+cmake -S $sourcePath "-B$buildPath" -GNinja "-DCMAKE_BUILD_TYPE=$buildType" -DCMAKE_C_COMPILER=cl -DCMAKE_CXX_COMPILER=cl "-DPYTHON_VERSION=$pythonVersion"
 cmake --build $buildPath -- -v 
 
 copy-item $buildPath/*.pyd "$sourcePath/pywinrt/winrt"
