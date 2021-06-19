3dd0fc569a6b5793df88a4177c7045a6baea108a
David Lechner <david@pybricks.com>
2021-06-18 20:26:22 -0500

    test/python: add buffer test
    This tests both Windows.Storage.Streams.IBuffer and
    Windows.Foundation.IMemoryBuffer + Windows.Foundation.IMemoryBufferReference

---
+++ src/test/python/test_buffer.py
@@ -0,0 +1,39 @@
+import sys
+sys.path.append("./generated")
+sys.path.append("../../package/pywinrt/projection/pywinrt")
+
+import unittest
+
+import winrt.windows.foundation as wf
+import winrt.windows.storage.streams as wss
+
+
+class TestBuffer(unittest.TestCase):
+    def test_new(self):
+        buf = wss.Buffer(5)
+        self.assertEqual(buf.length, 0)
+        self.assertEqual(buf.capacity, 5)
+
+    def test_buffer_protocol(self):
+        buf = wss.Buffer(5)
+        with memoryview(buf) as mv:
+            self.assertEqual(len(mv), 0)
+        
+        buf.length = 5
+        with memoryview(buf) as mv:
+            self.assertEqual(len(mv), 5)
+
+    def test_memory_buffer(self):
+        buf = wss.Buffer(5)
+        buf.length =  5
+        data = b"ABCDE"
+
+        with memoryview(buf) as mv:
+            mv[:] = data
+
+        mb = wss.Buffer.create_memory_buffer_over_i_buffer(buf)
+        with mb.create_reference() as mbr, memoryview(mbr) as mv:
+            self.assertEqual(mv, data)
+
+if __name__ == '__main__':
+    unittest.main()
