90cf386ffd4467319a3c81c2098fc6e5e03de3cc
David Lechner <david@pybricks.com>
2021-12-30 17:16:13 -0600

    pywinrt: add basic test for DataReader

---
+++ src/test/python/test_data_reader.py
@@ -0,0 +1,25 @@
+import struct
+import sys
+import unittest
+
+sys.path.append("./generated")
+sys.path.append("../../package/pywinrt/projection/pywinrt")
+
+import winrt.windows.storage.streams as wss
+
+
+class TestBuffer(unittest.TestCase):
+    def test_from_buffer(self):
+        SIZE = 5
+
+        buf = wss.Buffer(SIZE)
+        buf.length = SIZE
+        struct.pack_into("b" * SIZE, buf, 0, *range(SIZE))
+
+        reader = wss.DataReader.from_buffer(buf)
+
+        for i in range(SIZE):
+            self.assertEqual(reader.read_byte(), i)
+
+if __name__ == '__main__':
+    unittest.main()
