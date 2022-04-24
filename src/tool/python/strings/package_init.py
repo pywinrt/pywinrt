c6ab59bb9d89d8cdb2f3f9e7bd6ae3d853b5bf4a
David Lechner <david@pybricks.com>
2022-04-24 15:31:11 -0500

    package_init.py: make mypy happy
    the type hints say that spec could be None but this shouldn't actually happen

--- src/tool/python/strings/package_init.py
+++ src/tool/python/strings/package_init.py
@@ -8,6 +8,7 @@ def _import_ns_module(ns: str) -> ModuleType:
     module_name = f"_%_{ns.replace('.', '_')}"
     loader = ExtensionFileLoader(module_name, _winrt.__file__)
     spec = spec_from_loader(module_name, loader)
+    assert spec is not None
     module = module_from_spec(spec)
     loader.exec_module(module)
     return module
