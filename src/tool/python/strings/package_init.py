da096962c50b486a0736ab2ead8f7c24c54fc2c6
David Lechner <david@pybricks.com>
2022-04-24 02:00:12 -0500

    typing: add type hints to _import_ns_module()

--- src/tool/python/strings/package_init.py
+++ src/tool/python/strings/package_init.py
@@ -1,9 +1,10 @@
 from importlib.machinery import ExtensionFileLoader
 from importlib.util import spec_from_loader, module_from_spec
+from types import ModuleType
 
 from . import _winrt
 
-def _import_ns_module(ns):
+def _import_ns_module(ns: str) -> ModuleType:
     module_name = f"_%_{ns.replace('.', '_')}"
     loader = ExtensionFileLoader(module_name, _winrt.__file__)
     spec = spec_from_loader(module_name, loader)
