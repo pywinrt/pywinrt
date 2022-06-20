c62a49871b02260e3a4bb47caa6b92df05405d2c
David Lechner <david@pybricks.com>
2022-06-19 19:47:06 -0500

    limit parallel builds
    Building the generated code uses crazy big precompiled headers so it is common for the compiler to run out of heap space. We can try to prevent this by limiting the number of parallel builds.

--- src/package/pywinrt/projection/build.ps1
+++ src/package/pywinrt/projection/build.ps1
@@ -11,6 +11,6 @@ $sourcePath = $PSScriptRoot.Replace('\', '/')
 $buildPath = "$repoRootPath/_build/py-projection/$env:VSCMD_ARG_TGT_ARCH-$buildType"
 
 cmake -S $sourcePath "-B$buildPath" -GNinja "-DCMAKE_BUILD_TYPE=$buildType" -DCMAKE_C_COMPILER=cl -DCMAKE_CXX_COMPILER=cl "-DPYTHON_VERSION=$pythonVersion"
-cmake --build $buildPath -- -v 
+cmake --build $buildPath -- -v -j 4
 
 copy-item $buildPath/*.pyd "$sourcePath/pywinrt/winrt"
