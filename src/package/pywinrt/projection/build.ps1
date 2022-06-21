9cd6128da7c65c59ef34202af14786d66864e341
David Lechner <david@pybricks.com>
2022-06-21 00:36:00 -0500

    build.ps1: add compiler option
    This adds an option to override the compiler in the build.ps1 script.

--- src/package/pywinrt/projection/build.ps1
+++ src/package/pywinrt/projection/build.ps1
@@ -3,14 +3,18 @@ param (
     [string]$buildType = "Debug",
 
     [Parameter(Mandatory=$false)]
-    [string]$pythonVersion = "3.7"
+    [string]$pythonVersion = "3.7",
+
+    [Parameter(Mandatory=$false)]
+    [string]$compiler = "cl.exe"
 )
 
 $repoRootPath = (get-item $PSScriptRoot).parent.Parent.parent.Parent.FullName.Replace('\', '/')
 $sourcePath = $PSScriptRoot.Replace('\', '/')
 $buildPath = "$repoRootPath/_build/py-projection/$env:VSCMD_ARG_TGT_ARCH-$buildType"
 
-cmake -S $sourcePath "-B$buildPath" -GNinja "-DCMAKE_BUILD_TYPE=$buildType" -DCMAKE_C_COMPILER=cl -DCMAKE_CXX_COMPILER=cl "-DPYTHON_VERSION=$pythonVersion"
+
+cmake -S $sourcePath "-B$buildPath" -GNinja "-DCMAKE_BUILD_TYPE=$buildType" "-DCMAKE_C_COMPILER=$compiler" "-DCMAKE_CXX_COMPILER=$compiler" "-DPYTHON_VERSION=$pythonVersion"
 cmake --build $buildPath -- -v -j 4
 
 copy-item $buildPath/*.pyd "$sourcePath/pywinrt/winrt"
