0f30804a40d81dabf21d8a1de4750a3d403a0c62
David Lechner <david@lechnology.com>
2023-08-27 16:13:32 -0500

    projection/winrt-runtime: get building with cibuildwheel

--- projection/winrt-runtime/setup.py
+++ projection/winrt-runtime/setup.py
@@ -7,9 +7,8 @@ setup(
             "winrt._winrt",
             sources=["_winrt.cpp", "_winrt_array.cpp", "runtime.cpp"],
             include_dirs=get_include_dirs(),
-            define_macros=[("WIN32", None), ("_WINDOWS", None)],
-            extra_compile_args=["/std:c++20", "/permissive-", "/MT"],
-            extra_link_args=["kernel32.lib", "ole32.lib", "oleaut32.lib"],
+            extra_compile_args=["/std:c++20", "/permissive-"],
+            libraries=["windowsapp"],
         )
     ]
 )
