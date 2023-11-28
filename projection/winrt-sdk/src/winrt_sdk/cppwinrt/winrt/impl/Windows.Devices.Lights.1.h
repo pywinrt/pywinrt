ad50721e20a87895c9f2f96e1c5a57b706e67fba
David Lechner <david@lechnology.com>
2023-11-27 22:52:19 -0600

    run cppwinrt with latest 22621 updates

--- projection/winrt-sdk/src/winrt_sdk/cppwinrt/winrt/impl/Windows.Devices.Lights.1.h
+++ projection/winrt-sdk/src/winrt_sdk/cppwinrt/winrt/impl/Windows.Devices.Lights.1.h
@@ -22,6 +22,13 @@ WINRT_EXPORT namespace winrt::Windows::Devices::Lights
         ILampArray(std::nullptr_t = nullptr) noexcept {}
         ILampArray(void* ptr, take_ownership_from_abi_t) noexcept : winrt::Windows::Foundation::IInspectable(ptr, take_ownership_from_abi) {}
     };
+    struct WINRT_IMPL_EMPTY_BASES ILampArray2 :
+        winrt::Windows::Foundation::IInspectable,
+        impl::consume_t<ILampArray2>
+    {
+        ILampArray2(std::nullptr_t = nullptr) noexcept {}
+        ILampArray2(void* ptr, take_ownership_from_abi_t) noexcept : winrt::Windows::Foundation::IInspectable(ptr, take_ownership_from_abi) {}
+    };
     struct WINRT_IMPL_EMPTY_BASES ILampArrayStatics :
         winrt::Windows::Foundation::IInspectable,
         impl::consume_t<ILampArrayStatics>
