682892bba3f3e2a007145bf7dd32f1c0cffd3088
David Lechner <david@lechnology.com>
2024-03-28 22:26:15 -0500

    projection: fix buiding source package with non-msvc
    Fixes: https://github.com/pywinrt/pywinrt/issues/44

--- projection/winrt-Windows.Services.Store/setup.py
+++ projection/winrt-Windows.Services.Store/setup.py
@@ -1,15 +1,27 @@
 # WARNING: Please don't edit this file. It was automatically generated.
 
 from setuptools import Extension, setup
+from setuptools.command.build_ext import build_ext
 from winrt_sdk import get_include_dirs
 
+
+class build_ext_ex(build_ext):
+    def build_extension(self, ext):
+        if self.compiler.compiler_type == "msvc":
+            ext.extra_compile_args = ["/std:c++20", "/permissive-"]
+        else:
+            ext.extra_compile_args = ["-std=c++20"]
+
+        build_ext.build_extension(self, ext)
+
+
 setup(
+    cmdclass = {'build_ext': build_ext_ex},
     ext_modules=[
         Extension(
             "winrt._winrt_windows_services_store",
             sources=["py.Windows.Services.Store.cpp"],
             include_dirs=get_include_dirs(),
-            extra_compile_args=["/std:c++20", "/permissive-"],
             libraries=["windowsapp"],
         )
     ],
