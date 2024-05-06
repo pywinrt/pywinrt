09b7a32b55329b630d23d7928cfe81e91cc44607
David Lechner <david@lechnology.com>
2024-05-06 06:18:07 -0500

    projection: add __all__ in graphics.capture.interop

--- projection/interop/winrt-Windows.Graphics.Capture.Interop/winrt/windows/graphics/capture/interop/__init__.py
+++ projection/interop/winrt-Windows.Graphics.Capture.Interop/winrt/windows/graphics/capture/interop/__init__.py
@@ -1,4 +1,6 @@
 from ..... import _winrt_windows_graphics_capture_interop
 
+__all__ = ["create_for_monitor", "create_for_window"]
+
 create_for_monitor = _winrt_windows_graphics_capture_interop.create_for_monitor
 create_for_window = _winrt_windows_graphics_capture_interop.create_for_window
