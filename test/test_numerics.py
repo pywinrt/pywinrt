c25d01b4774c8b9cc551692be69120e926b4b792
David Lechner <david@pybricks.com>
2024-07-27 10:04:10 -0500

    test: add inplace operator tests for custom numerics
    The default implementation works for these, so we don't have to wrap
    the C++ inplace operators.

--- test/test_numerics.py
+++ test/test_numerics.py
@@ -3,6 +3,13 @@ import unittest
 import winrt.windows.foundation.numerics as wfn
 
 
+# TODO: replace this when inverse method is implemented on Quaternion
+def inverse(q: wfn.Quaternion) -> wfn.Quaternion:
+    return wfn.Quaternion(-q.x, -q.y, -q.z, q.w) * (
+        1 / (q.x**2 + q.y**2 + q.z**2 + q.w**2)
+    )
+
+
 class TestNumerics(unittest.TestCase):
     def test_struct_ctor_pos(self):
         r = wfn.Rational(2, 4)
@@ -320,12 +327,6 @@ class TestNumerics(unittest.TestCase):
             wfn.Vector4(1, 2, 3, 4) / 5, wfn.Vector4(1 / 5, 2 / 5, 3 / 5, 4 / 5)
         )
 
-        # TODO: replace this when inverse method is implemented on Quaternion
-        def inverse(q: wfn.Quaternion) -> wfn.Quaternion:
-            return wfn.Quaternion(-q.x, -q.y, -q.z, q.w) * (
-                1 / (q.x**2 + q.y**2 + q.z**2 + q.w**2)
-            )
-
         self.assertEqual(
             wfn.Quaternion(1, 2, 3, 4) / wfn.Quaternion(5, 6, 7, 8),
             wfn.Quaternion(1, 2, 3, 4) * inverse(wfn.Quaternion(5, 6, 7, 8)),
@@ -389,3 +390,169 @@ class TestNumerics(unittest.TestCase):
             ),
         )
         self.assertEqual(-wfn.Quaternion(1, 2, 3, 4), wfn.Quaternion(-1, -2, -3, -4))
+
+    def test_iadd(self):
+        v = wfn.Vector2(1, 2)
+        v += wfn.Vector2(3, 4)
+        self.assertEqual(v, wfn.Vector2(4, 6))
+
+        v = wfn.Vector3(1, 2, 3)
+        v += wfn.Vector3(4, 5, 6)
+        self.assertEqual(v, wfn.Vector3(5, 7, 9))
+
+        v = wfn.Vector4(1, 2, 3, 4)
+        v += wfn.Vector4(5, 6, 7, 8)
+        self.assertEqual(v, wfn.Vector4(6, 8, 10, 12))
+
+        m = wfn.Matrix3x2(1, 2, 3, 4, 5, 6)
+        m += wfn.Matrix3x2(7, 8, 9, 10, 11, 12)
+        self.assertEqual(m, wfn.Matrix3x2(8, 10, 12, 14, 16, 18))
+
+        m = wfn.Matrix4x4(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
+        m += wfn.Matrix4x4(
+            17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32
+        )
+        self.assertEqual(
+            m,
+            wfn.Matrix4x4(
+                18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48
+            ),
+        )
+
+        q = wfn.Quaternion(1, 2, 3, 4)
+        q += wfn.Quaternion(5, 6, 7, 8)
+        self.assertEqual(q, wfn.Quaternion(6, 8, 10, 12))
+
+    def test_isub(self):
+        v = wfn.Vector2(1, 2)
+        v -= wfn.Vector2(3, 4)
+        self.assertEqual(v, wfn.Vector2(-2, -2))
+
+        v = wfn.Vector3(1, 2, 3)
+        v -= wfn.Vector3(4, 5, 6)
+        self.assertEqual(v, wfn.Vector3(-3, -3, -3))
+
+        v = wfn.Vector4(1, 2, 3, 4)
+        v -= wfn.Vector4(5, 6, 7, 8)
+        self.assertEqual(v, wfn.Vector4(-4, -4, -4, -4))
+
+        m = wfn.Matrix3x2(1, 2, 3, 4, 5, 6)
+        m -= wfn.Matrix3x2(7, 8, 9, 10, 11, 12)
+        self.assertEqual(m, wfn.Matrix3x2(-6, -6, -6, -6, -6, -6))
+
+        m = wfn.Matrix4x4(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
+        m -= wfn.Matrix4x4(
+            17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32
+        )
+        self.assertEqual(
+            m,
+            wfn.Matrix4x4(*[-16] * 16),
+        )
+
+        q = wfn.Quaternion(1, 2, 3, 4)
+        q -= wfn.Quaternion(5, 6, 7, 8)
+        self.assertEqual(q, wfn.Quaternion(-4, -4, -4, -4))
+
+    def test_imul(self):
+        v = wfn.Vector2(1, 2)
+        v *= wfn.Vector2(3, 4)
+        self.assertEqual(v, wfn.Vector2(3, 8))
+
+        v = wfn.Vector2(1, 2)
+        v *= 3
+        self.assertEqual(v, wfn.Vector2(3, 6))
+
+        v = wfn.Vector3(1, 2, 3)
+        v *= wfn.Vector3(4, 5, 6)
+        self.assertEqual(v, wfn.Vector3(4, 10, 18))
+
+        v = wfn.Vector3(1, 2, 3)
+        v *= 4
+        self.assertEqual(v, wfn.Vector3(4, 8, 12))
+
+        v = wfn.Vector4(1, 2, 3, 4)
+        v *= wfn.Vector4(5, 6, 7, 8)
+        self.assertEqual(v, wfn.Vector4(5, 12, 21, 32))
+
+        v = wfn.Vector4(1, 2, 3, 4)
+        v *= 5
+        self.assertEqual(v, wfn.Vector4(5, 10, 15, 20))
+
+        m = wfn.Matrix3x2(1, 2, 3, 4, 5, 6)
+        m *= wfn.Matrix3x2(7, 8, 9, 10, 11, 12)
+        self.assertEqual(m, wfn.Matrix3x2(25, 28, 57, 64, 100, 112))
+
+        m = wfn.Matrix3x2(1, 2, 3, 4, 5, 6)
+        m *= 2
+        self.assertEqual(m, wfn.Matrix3x2(2, 4, 6, 8, 10, 12))
+
+        m = wfn.Matrix4x4(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
+        m *= wfn.Matrix4x4(
+            17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32
+        )
+        self.assertEqual(
+            m,
+            wfn.Matrix4x4(
+                250,
+                260,
+                270,
+                280,
+                618,
+                644,
+                670,
+                696,
+                986,
+                1028,
+                1070,
+                1112,
+                1354,
+                1412,
+                1470,
+                1528,
+            ),
+        )
+
+        m = wfn.Matrix4x4(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
+        m *= 2
+        self.assertEqual(
+            m, wfn.Matrix4x4(2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32)
+        )
+
+        q = wfn.Quaternion(1, 2, 3, 4)
+        q *= wfn.Quaternion(5, 6, 7, 8)
+        self.assertEqual(q, wfn.Quaternion(24, 48, 48, -6))
+
+        q = wfn.Quaternion(1, 2, 3, 4)
+        q *= 2
+        self.assertEqual(q, wfn.Quaternion(2, 4, 6, 8))
+
+    def test_itruediv(self):
+        v = wfn.Vector2(1, 2)
+        v /= wfn.Vector2(3, 4)
+        self.assertEqual(v, wfn.Vector2(1 / 3, 2 / 4))
+
+        v = wfn.Vector2(1, 2)
+        v /= 3
+        self.assertEqual(v, wfn.Vector2(1 / 3, 2 / 3))
+
+        v = wfn.Vector3(1, 2, 3)
+        v /= wfn.Vector3(4, 5, 6)
+        self.assertEqual(v, wfn.Vector3(1 / 4, 2 / 5, 3 / 6))
+
+        v = wfn.Vector3(1, 2, 3)
+        v /= 4
+        self.assertEqual(v, wfn.Vector3(1 / 4, 2 / 4, 3 / 4))
+
+        v = wfn.Vector4(1, 2, 3, 4)
+        v /= wfn.Vector4(5, 6, 7, 8)
+        self.assertEqual(v, wfn.Vector4(1 / 5, 2 / 6, 3 / 7, 4 / 8))
+
+        v = wfn.Vector4(1, 2, 3, 4)
+        v /= 5
+        self.assertEqual(v, wfn.Vector4(1 / 5, 2 / 5, 3 / 5, 4 / 5))
+
+        q = wfn.Quaternion(1, 2, 3, 4)
+        q /= wfn.Quaternion(5, 6, 7, 8)
+        self.assertEqual(
+            q, wfn.Quaternion(1, 2, 3, 4) * inverse(wfn.Quaternion(5, 6, 7, 8))
+        )
