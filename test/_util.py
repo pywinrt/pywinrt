3bc71b5fe41f14e646c820352e2c7487e3b1acf6
David Lechner <david@lechnology.com>
2024-10-28 22:59:14 -0500

    tests: fix async_test() type hints

--- test/_util.py
+++ test/_util.py
@@ -4,11 +4,13 @@ Helper functions shared by tests.
 
 import asyncio
 
+from typing import Any, Callable
 
-def async_test(test):
+
+def async_test(test: Callable[..., Any]) -> Callable[..., None]:
     """
     Decorator to run async test methods.
     """
-    def wrapper(*args, **kwargs):
+    def wrapper(*args: Any, **kwargs: Any) -> None:
         asyncio.run(test(*args, **kwargs))
     return wrapper
