c3e2667f521b8d7d77d6961b422b5e6698b910b4
David Lechner <david@pybricks.com>
2024-10-31 19:01:56 -0500

    interop: don't use type aliases for Microsoft.UI.Interop

--- projection/interop/winrt-Microsoft.UI.Interop/winrt/microsoft/ui/interop/__init__.py
+++ projection/interop/winrt-Microsoft.UI.Interop/winrt/microsoft/ui/interop/__init__.py
@@ -1,4 +1,11 @@
-from .... import _winrt_microsoft_ui_interop as interop
+from ...._winrt_microsoft_ui_interop import (
+    get_window_id_from_window,
+    get_window_from_window_id,
+    get_display_id_from_monitor,
+    get_monitor_from_display_id,
+    get_icon_id_from_icon,
+    get_icon_from_icon_id,
+)
 
 __all__ = [
     "get_window_id_from_window",
@@ -8,11 +15,3 @@ __all__ = [
     "get_icon_id_from_icon",
     "get_icon_from_icon_id",
 ]
-
-
-get_window_id_from_window = interop.get_window_id_from_window
-get_window_from_window_id = interop.get_window_from_window_id
-get_display_id_from_monitor = interop.get_display_id_from_monitor
-get_monitor_from_display_id = interop.get_monitor_from_display_id
-get_icon_id_from_icon = interop.get_icon_id_from_icon
-get_icon_from_icon_id = interop.get_icon_from_icon_id
