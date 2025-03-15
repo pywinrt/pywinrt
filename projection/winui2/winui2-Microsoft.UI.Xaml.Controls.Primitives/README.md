ba29207f60f41aaab5d7a3de4637254736663eb9
David Lechner <david@pybricks.com>
2025-03-15 17:33:02 -0500

    projection: generate winui2 project files

---
+++ projection/winui2/winui2-Microsoft.UI.Xaml.Controls.Primitives/README.md
@@ -0,0 +1,7 @@
+<!-- warning: Please don't edit this file. It was automatically generated. -->
+
+# winui2-Microsoft.UI.Xaml.Controls.Primitives
+
+Windows Runtime (WinRT) APIs for for the `winui2-Microsoft.UI.Xaml.Controls.Primitives` namespace.
+
+This package provides the `winui2.microsoft.ui.xaml.controls.primitives` module.
---
+++ projection/winui2/winui2-Microsoft.UI.Xaml.Controls.Primitives/setup.py
@@ -0,0 +1,32 @@
+# WARNING: Please don't edit this file. It was automatically generated.
+
+from setuptools import Extension, setup
+from setuptools.command.build_ext import build_ext
+from winrt_sdk import get_include_dirs
+from winrt_microsoft_ui_xaml import get_include_dirs as get_winui2_include_dirs
+
+
+class build_ext_ex(build_ext):
+    def build_extension(self, ext):
+        if self.compiler.compiler_type == "msvc":
+            ext.extra_compile_args = ["/std:c++20", "/permissive-"]
+        elif self.compiler.compiler_type == "mingw32":
+            ext.extra_compile_args = ["-std=c++20", "-D_WIN32_WINNT=_WIN32_WINNT_WIN10"]
+            ext.extra_link_args = ["-loleaut32"]
+        else:
+            raise ValueError(f"Unsupported compiler: {self.compiler.compiler_type}")
+
+        build_ext.build_extension(self, ext)
+
+
+setup(
+    cmdclass={"build_ext": build_ext_ex},
+    ext_modules=[
+        Extension(
+            "winui2._winui2_microsoft_ui_xaml_controls_primitives",
+            sources=["py.Microsoft.UI.Xaml.Controls.Primitives.cpp"],
+            include_dirs=get_include_dirs() + get_winui2_include_dirs(),
+            libraries=["windowsapp"],
+        )
+    ],
+)
