47259981c2ee40808c3e3124bf89bcdd0a56deca
David Lechner <david@pybricks.com>
2025-03-15 17:55:13 -0500

    projection: fix ci include paths for webview2

--- projection/winrt-WindowsAppSDK/src/winrt_windows_app_sdk/__init__.py
+++ projection/winrt-WindowsAppSDK/src/winrt_windows_app_sdk/__init__.py
@@ -15,7 +15,14 @@ def get_include_dirs() -> List[str]:
         webview2_dir = files("webview2")
     except ModuleNotFoundError:
         # hack for building on CI without binary packages
-        webview2_dir = package_dir.joinpath("..", "..", "..", "webview2")
+        webview2_dir = package_dir.joinpath(
+            "..",
+            "..",
+            "..",
+            "webview2",
+            "webview2-Microsoft.Web.WebView2.Core",
+            "webview2",
+        )
 
     return [str(package_dir.joinpath(d)) for d in ["cppwinrt", "pywinrt"]] + [
         str(webview2_dir.joinpath(d)) for d in ["cppwinrt", "pywinrt"]
