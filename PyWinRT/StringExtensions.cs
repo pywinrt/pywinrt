231e4b10ecff90256c1a4c9477b4b18d441299d4
David Lechner <david@pybricks.com>
2026-09-13 16:09:09 -0500

    PyWinRT: speed up Python identifier conversion
    Use source-generated regexes and cache the results. The static
    Regex.Replace() cache only holds 15 entries, so the 28 patterns used here
    thrashed it on every call and dominated the runtime of the tool.

--- PyWinRT/StringExtensions.cs
+++ PyWinRT/StringExtensions.cs
@@ -1,11 +1,11 @@
-using System.Collections.ObjectModel;
+using System.Collections.Concurrent;
 using System.Text;
 using System.Text.RegularExpressions;
 
-static class StringExtensions
+static partial class StringExtensions
 {
     // https://docs.python.org/3/reference/lexical_analysis.html#keywords
-    internal static readonly ReadOnlyCollection<string> pythonKeywords =
+    internal static readonly HashSet<string> pythonKeywords =
         new(
             [
                 "and",
@@ -43,77 +43,174 @@ static class StringExtensions
             ]
         );
 
+    // These conversions are done many times for the same identifiers
+    // (once per generated file per use), so the results are cached.
+    private static readonly ConcurrentDictionary<string, string> pythonIdentifierCache =
+        new(Environment.ProcessorCount * 4, 1 << 16);
+
+    private static readonly ConcurrentDictionary<string, string> pythonConstantCache =
+        new(Environment.ProcessorCount * 4, 1 << 14);
+
+    // Regexes applied in order by ToSnakeCase(). These are source-generated
+    // and applied in order because the built-in static Regex.Replace() cache
+    // only holds 15 entries, so using it here thrashed the cache and
+    // dominated the runtime of the tool.
+
+    // Replace acronyms
+    [GeneratedRegex(@"^([A-Z])([A-Z0-9]+)$")]
+    private static partial Regex AcronymRegex();
+
+    // Replace memory size
+    [GeneratedRegex(@"(\d+)(M|G)B")]
+    private static partial Regex MemorySizeRegex();
+
+    // Replace 3MF with _3mf
+    [GeneratedRegex(@"3MF")]
+    private static partial Regex ThreeMfRegex();
+
+    // Replace ARM with Arm
+    [GeneratedRegex(@"ARM")]
+    private static partial Regex ArmRegex();
+
+    // replace DB with Db, ignore ID
+    [GeneratedRegex(@"(?<!I)DB")]
+    private static partial Regex DbRegex();
+
+    // Replace DRM with Drm
+    [GeneratedRegex(@"DRM")]
+    private static partial Regex DrmRegex();
+
+    // Replace DirectX with Directx
+    [GeneratedRegex(@"DirectX")]
+    private static partial Regex DirectXRegex();
+
+    // replace D3D with D3d
+    [GeneratedRegex(@"(D(?:irect)?)3D")]
+    private static partial Regex D3DRegex();
+
+    // Replace 3D with _3d
+    [GeneratedRegex(@"(?<!\d)3D(?!ay)")]
+    private static partial Regex ThreeDRegex();
+
+    // Replace DOM with Dom
+    [GeneratedRegex(@"DOM")]
+    private static partial Regex DomRegex();
+
+    // Replace EU with Eu
+    [GeneratedRegex(@"EU")]
+    private static partial Regex EuRegex();
+
+    // Replace ID with Id, ignore UI
+    [GeneratedRegex(@"(?<![A-Z])ID")]
+    private static partial Regex IdRegex();
+
+    // Replace IO with Io
+    [GeneratedRegex(@"(?<![A-Z])IO")]
+    private static partial Regex IoRegex();
+
+    // Replace iOS with Ios
+    [GeneratedRegex(@"iOS")]
+    private static partial Regex IosRegex();
+
+    // Replace IP with Ip, ignore UI
+    [GeneratedRegex(@"(?<!U)IP(?=[A-Zv]|$)")]
+    private static partial Regex IpRegex();
+
+    // Replace MD5 with Md5
+    [GeneratedRegex(@"MD5")]
+    private static partial Regex Md5Regex();
+
+    // Replace ML with Ml
+    [GeneratedRegex(@"ML")]
+    private static partial Regex MlRegex();
+
+    // Replace NS with Ns
+    [GeneratedRegex(@"NS")]
+    private static partial Regex NsRegex();
+
+    // Replace NT with Nt
+    [GeneratedRegex(@"NT")]
+    private static partial Regex NtRegex();
+
+    // Replace OEM with Oem
+    [GeneratedRegex(@"OEM")]
+    private static partial Regex OemRegex();
+
+    // Replace OK with Ok
+    [GeneratedRegex(@"OK")]
+    private static partial Regex OkRegex();
+
+    // Replace OS with Os
+    [GeneratedRegex(@"(?<![A-Z])OS")]
+    private static partial Regex OsRegex();
+
+    // Replace PPC with Ppc
+    [GeneratedRegex(@"PPC")]
+    private static partial Regex PpcRegex();
+
+    // Replace PC with Pc
+    [GeneratedRegex(@"PC")]
+    private static partial Regex PcRegex();
+
+    // Replace UI with Ui (also handles UInt)
+    [GeneratedRegex(@"UI(?!nfo)")]
+    private static partial Regex UiRegex();
+
+    // Replace UWP with Uwp
+    [GeneratedRegex(@"UWP")]
+    private static partial Regex UwpRegex();
+
+    // Replace WebView2 with Webview2
+    [GeneratedRegex(@"WebView2")]
+    private static partial Regex WebView2Regex();
+
+    // Fix up interface prefix, also fixes IR, IM, IBeam
+    [GeneratedRegex(@"(?<![A-Z])I([A-Z])(?!im)")]
+    private static partial Regex InterfacePrefixRegex();
+
+    private static readonly MatchEvaluator acronymEvaluator = m =>
+        $"{m.Groups[1].Value}{m.Groups[2].Value.ToLowerInvariant()}";
+
+    private static readonly MatchEvaluator interfacePrefixEvaluator = m =>
+        $"I{m.Groups[1].Value.ToLowerInvariant()}";
+
     static string ToSnakeCase(this string str)
     {
-        var sb = new StringBuilder();
+        str = AcronymRegex().Replace(str, acronymEvaluator);
+        str = MemorySizeRegex().Replace(str, "_${1}${2}b");
+        str = ThreeMfRegex().Replace(str, "_3mf");
+        str = ArmRegex().Replace(str, "Arm");
+        str = DbRegex().Replace(str, "Db");
+        str = DrmRegex().Replace(str, "Drm");
+        str = DirectXRegex().Replace(str, "Directx");
+        str = D3DRegex().Replace(str, "${1}3d");
+        str = ThreeDRegex().Replace(str, "_3d");
+        str = DomRegex().Replace(str, "Dom");
+        str = EuRegex().Replace(str, "Eu");
+        str = IdRegex().Replace(str, "Id");
+        str = IoRegex().Replace(str, "Io");
+        str = IosRegex().Replace(str, "Ios");
+        str = IpRegex().Replace(str, "Ip");
+        str = Md5Regex().Replace(str, "Md5");
+        str = MlRegex().Replace(str, "Ml");
+        str = NsRegex().Replace(str, "Ns");
+        str = NtRegex().Replace(str, "Nt");
+        str = OemRegex().Replace(str, "Oem");
+        str = OkRegex().Replace(str, "Ok");
+        str = OsRegex().Replace(str, "Os");
+        str = PpcRegex().Replace(str, "Ppc");
+        str = PcRegex().Replace(str, "Pc");
+        str = UiRegex().Replace(str, "Ui");
+        str = UwpRegex().Replace(str, "Uwp");
+        str = WebView2Regex().Replace(str, "Webview2");
+        str = InterfacePrefixRegex().Replace(str, interfacePrefixEvaluator);
 
-        // Replace acronyms
-        str = Regex.Replace(
-            str,
-            @"^([A-Z])([A-Z0-9]+)$",
-            m => $"{m.Groups[1].Value}{m.Groups[2].Value.ToLowerInvariant()}"
-        );
-        // Replace memory size
-        str = Regex.Replace(str, @"(\d+)(M|G)B", "_${1}${2}b");
-        // Replace 3MF with _3mf
-        str = Regex.Replace(str, @"3MF", "_3mf");
-        // Replace ARM with Arm
-        str = Regex.Replace(str, @"ARM", "Arm");
-        // replace DB with Db, ignore ID
-        str = Regex.Replace(str, @"(?<!I)DB", "Db");
-        // Replace DRM with Drm
-        str = Regex.Replace(str, @"DRM", "Drm");
-        // Replace DirectX with Directx
-        str = Regex.Replace(str, @"DirectX", "Directx");
-        // replace D3D with D3d
-        str = Regex.Replace(str, @"(D(?:irect)?)3D", "${1}3d");
-        // Replace 3D with _3d
-        str = Regex.Replace(str, @"(?<!\d)3D(?!ay)", "_3d");
-        // Replace DOM with Dom
-        str = Regex.Replace(str, @"DOM", "Dom");
-        // Replace EU with Eu
-        str = Regex.Replace(str, @"EU", "Eu");
-        // Replace ID with Id, ignore UI
-        str = Regex.Replace(str, @"(?<![A-Z])ID", "Id");
-        // Replace IO with Io
-        str = Regex.Replace(str, @"(?<![A-Z])IO", "Io");
-        // Replace iOS with Ios
-        str = Regex.Replace(str, @"iOS", "Ios");
-        // Replace IP with Ip, ignore UI
-        str = Regex.Replace(str, @"(?<!U)IP(?=[A-Zv]|$)", "Ip");
-        // Replace MD5 with Md5
-        str = Regex.Replace(str, @"MD5", "Md5");
-        // Replace ML with Ml
-        str = Regex.Replace(str, @"ML", "Ml");
-        // Replace NS with Ns
-        str = Regex.Replace(str, @"NS", "Ns");
-        // Replace NT with Nt
-        str = Regex.Replace(str, @"NT", "Nt");
-        // Replace OEM with Oem
-        str = Regex.Replace(str, @"OEM", "Oem");
-        // Replace OK with Ok
-        str = Regex.Replace(str, @"OK", "Ok");
-        // Replace OS with Os
-        str = Regex.Replace(str, @"(?<![A-Z])OS", "Os");
-        // Replace PPC with Ppc
-        str = Regex.Replace(str, @"PPC", "Ppc");
-        // Replace PC with Pc
-        str = Regex.Replace(str, @"PC", "Pc");
-        // Replace UI with Ui (also handles UInt)
-        str = Regex.Replace(str, @"UI(?!nfo)", "Ui");
-        // Replace UWP with Uwp
-        str = Regex.Replace(str, @"UWP", "Uwp");
-        // Replace WebView2 with Webview2
-        str = Regex.Replace(str, @"WebView2", "Webview2");
-        // Fix up interface prefix, also fixes IR, IM, IBeam
-        str = Regex.Replace(
-            str,
-            @"(?<![A-Z])I([A-Z])(?!im)",
-            m => $"I{m.Groups[1].Value.ToLowerInvariant()}"
-        );
+        var sb = new StringBuilder(str.Length + 8);
 
-        foreach (var (i, c) in str.Select((c, i) => (i, c)))
+        for (var i = 0; i < str.Length; i++)
         {
+            var c = str[i];
+
             if (i > 0 && char.IsUpper(c) && str[i - 1] != '_')
             {
                 sb.Append('_');
@@ -133,12 +230,7 @@ static class StringExtensions
     /// </remarks>
     public static string ToPythonIdentifier(this string str, bool isTypeMethod = false)
     {
-        var identifier = str.ToSnakeCase().ToLowerInvariant();
-
-        if (pythonKeywords.Contains(identifier))
-        {
-            return $"{identifier}_";
-        }
+        var identifier = pythonIdentifierCache.GetOrAdd(str, ToPythonIdentifierUncached);
 
         // types have a method named mro, so for metaclasses, we have to avoid this
         if (isTypeMethod && identifier == "mro")
@@ -149,7 +241,20 @@ static class StringExtensions
         return identifier;
     }
 
-    public static string ToPythonConstant(this string str) => str.ToSnakeCase().ToUpperInvariant();
+    private static string ToPythonIdentifierUncached(string str)
+    {
+        var identifier = str.ToSnakeCase().ToLowerInvariant();
+
+        if (pythonKeywords.Contains(identifier))
+        {
+            return $"{identifier}_";
+        }
+
+        return identifier;
+    }
+
+    public static string ToPythonConstant(this string str) =>
+        pythonConstantCache.GetOrAdd(str, static s => s.ToSnakeCase().ToUpperInvariant());
 
     /// <summary>
     /// Converts a WinRT dotted namespace to a C++- :: namespace.
